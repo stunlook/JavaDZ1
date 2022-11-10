578c53987a5b69cfb821fd72130d836d73248028
Stepan Smirnov <stunlooksteff@gmail.com>
2022-11-10 09:55:44 +0300

    Fix Code

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         int price = 250;
-        int amount_for_bonus = 20;
+        int amountForBonus = 20;
         int bonus = 1;
 
-        System.out.println("Начисленые мили: " + price / amount_for_bonus * bonus);
+        System.out.println("Начисленые мили: " + price / amountForBonus * bonus);
     }
 }
